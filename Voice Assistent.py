eb7018344e2151f4e45da688b67825ec24f75bb2
CristianChaves1097 <cchavessaldana@gmail.com>
2024-02-19 02:16:56 -0500

    Update Voice Assistent.py

--- Voice Assistent.py
+++ Voice Assistent.py
@@ -5,11 +5,13 @@ import webbrowser
 import os
 import tkinter as tk
 
+#convert text into speech. 
 def speak(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
 
+# It attempts to recognize speech using Google's Speech Recognition service
 def get_audio():
     recognizer = sr.Recognizer()
 
@@ -36,11 +38,19 @@ def assistant(command):
         print("youtube")
     elif "open google" in command:
         webbrowser.open("https://www.google.com/")
-    elif "open visual studio Code" in command:
-        os.system("code")
-    elif "tell me the time" in command:
+    elif "on google" in command and "search" in command:
+            search_url = "https://www.google.com/search?q="
+            search = command.replace("search", "").replace("on google", "").strip()
+            search = "+".join(search.split())
+            url = search_url + search
+            webbrowser.open(url)
+
+    elif "tell me the time" in command or "what time it is" in command:
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
         speak(f"The current time is {current_time}")
+    # elif "open" in command:
+    #     prog = command.replace("open", "").strip()
+    #     os.system(prog)
     elif "exit" in command or "quit" in command:
         speak("Goodbye!")
         exit()
@@ -48,7 +58,6 @@ def assistant(command):
         speak(f"I'm sorry, I don't understand that command.")
 
 if __name__ == "__main__":
-    import tkinter as tk
     window = tk.Tk()
     greeting = tk.Label(text="Hello! How can I assist you today?")
     greeting.pack()
